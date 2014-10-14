d4c4832153a3fff5721fb0ebd8919b4fceb11302
DEV <dev@DEVs-Mac-mini.local>
2014-10-14 17:05:41 +0300

    adds a router

--- app/js/lisa.js
+++ app/js/lisa.js
@@ -1,12 +1,16 @@
-/**
- * @jsx React.DOM
- */
- var HelloWorld = React.createClass({
- 	render: function() {
- 		return (
- 			<div>HelloWorld</div>
- 		);
- 	}
- });
-
- React.renderComponent(<HelloWorld/>,document.getElementById('content'));
+var AmpersandRouter = require('ampersand-router');
+Router = AmpersandRouter.extend({
+    routes: {
+        '': 'home',
+        'salam':'salam'
+    },
+    
+    home: function () {
+       alert(1);
+    },
+    salam:function  () {
+      alert(2);
+    }
+});
+var router = new Router();
+router.history.start({ pushState: true})
