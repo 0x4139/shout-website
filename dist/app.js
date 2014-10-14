e786efa34f532848a9ae73bc52130e79f6870201
DEV <dev@DEVs-Mac-mini.local>
2014-10-14 15:33:11 +0300

    updates the app.js in order to support any route

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,15 @@
 var express = require('express')
-var engines = require('consolidate');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname))
-app.engine('html', engines.hogan);
+app.engine('html', require('ejs').renderFile);
+
 app.set('views', __dirname); // specify the views directory
+
+app.get('*',function(req,res){
+	res.render('index.html');
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
